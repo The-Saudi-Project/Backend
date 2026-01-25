bee5094a5ba22a7a655a835527722df126a8e8c3
Shamal Khalid N P <shamalkhalidnp@gmail.com>
2026-01-25 20:41:08 +0530

    safari fix 3

--- src/routes.js
+++ src/routes.js
@@ -8,6 +8,8 @@ app.use(
   cors({
     origin: ["http://localhost:5173", "thesaudiproject.vercel.app"],
     credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
   }),
 );
 
